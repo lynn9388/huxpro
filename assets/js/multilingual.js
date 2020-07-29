b7774810856a46e3250547e8a5487e559025e804
Lynn <lynn9388@gmail.com>
2020-07-29 16:15:11 +0800

    Change default language of Language Selector to English

--- assets/js/multilingual.js
+++ assets/js/multilingual.js
@@ -1,42 +1,37 @@
-var $select = document.querySelector("select");
-var $zh = document.querySelector(".zh");
-var $en = document.querySelector(".en");
+let $select = document.querySelector("select");
+let $en = document.querySelector(".en");
+let $zh = document.querySelector(".zh");
 
 function getLang() {
-    return new URLSearchParams(document.location.search).get("lang");
+    let lang = new URLSearchParams(document.location.search).get("lang");
+    if (lang == null) {
+        lang = $select.options[$select.options.selectedIndex].value;
+    }
+    return lang;
 }
 
 function setLang(newLang) {
-    var params = new URLSearchParams(document.location.search);
+    let params = new URLSearchParams(document.location.search);
     params.set("lang", newLang);
     document.location.search = params.toString();
 }
 
 function render() {
-    var lang = getLang();
+    let lang = getLang();
     if (lang == "en") {
-        $select.selectedIndex = 1;
+        $select.selectedIndex = 0;
         $en.style.display = "block";
         $en.classList.add("active");
         $zh.style.display = "none";
         $zh.classList.remove("active");
-    } else {
-        $select.selectedIndex = 0;
+    } else if (lang == "zh") {
+        $select.selectedIndex = 1;
         $zh.style.display = "block";
         $zh.classList.add("active");
         $en.style.display = "none";
         $en.classList.remove("active");
     }
     generateCatalog(".catalog-body");
-}
-
-function onLanChange(index) {
-    if (index == 0) {
-        lang = "zh";
-    } else {
-        lang = "en";
-    }
-    setLang(lang);
 }
 
 render();
